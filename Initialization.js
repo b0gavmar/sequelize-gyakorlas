0441f7cb1c99fe891003ef9f340d2bb7f65e3457
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-01-24 12:18:12 +0100

    initialization

---
+++ Initialization.js
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+const { DataTypes } = Sequelize;
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./data/database.sqlite",
+});
+
+const users = sequelize.define("users", {
+  student_id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: VARCHAR(255),
+    allowNull: false,
+    validate: {
+      min: 4,
+      len: [4, 20],
+    },
+  },
+  favorite_class: {
+    type: VARCHAR(255),
+    default: "Computer Science",
+  },
+  school_year: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  has_language_examination: {
+    type: TINYINT,
+    default: true,
+  }
+});
